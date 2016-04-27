ffbb61e53f0a3b41723992df930fe8a1320de03c
h82258652 <h82258652@hotmail.com>
2016-04-28 07:53:34 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace BingoWallpaper.ViewModels
+{
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<DetailViewModel>();
+        }
+
+        public DetailViewModel Detail
+        {
+            get
+            {
+                return ServiceLocator.Current.GetInstance<DetailViewModel>();
+            }
+        }
+    }
+}
