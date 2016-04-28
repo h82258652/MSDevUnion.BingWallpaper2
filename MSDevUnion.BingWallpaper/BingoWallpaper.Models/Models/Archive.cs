b0c1cef1d3d91b8ef5718fb7a32561b841e6575c
h82258652 <h82258652@hotmail.com>
2016-04-28 08:56:55 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Models/Models/Archive.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BingoWallpaper.Models
+{
+    [JsonObject]
+    public class Archive : AVObject
+    {
+        [JsonProperty("date")]
+        public string Date
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("hs")]
+        public IReadOnlyList<Hotspot> Hotspots
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("cs")]
+        public CoverStory CoverStory
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("msg")]
+        public IReadOnlyList<Message> Messages
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("link")]
+        public string Link
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("info")]
+        public string Info
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("image")]
+        public LeanCloudPointer Image
+        {
+            get;
+            set;
+        }
+
+        [JsonProperty("market")]
+        public string Market
+        {
+            get;
+            set;
+        }
+    }
+}
