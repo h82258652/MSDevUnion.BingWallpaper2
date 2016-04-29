218abc1bcc955ea0df5e7fb23cbc7ceb117e3089
h82258652 <h82258652@hotmail.com>
2016-04-29 13:25:34 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.Services.UnitTests/WallpaperServiceTest.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Services.UnitTests/WallpaperServiceTest.cs
@@ -34,9 +34,31 @@ namespace BingoWallpaper.Services.UnitTests
             });
         }
 
-        //public async Task TestGetImagesAsync()
-        //{
-        //    throw new NotImplementedException();
-        //}
+        [Test]
+        public async Task TestGetImageAsync()
+        {
+            var image = await _service.GetImageAsync("559d0e88e4b03bd51879a0de");
+            Assert.AreEqual(image.ErrorCode, 0);
+            Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await _service.GetImageAsync(null);
+            });
+            Assert.ThrowsAsync<ArgumentException>(async () =>
+            {
+                await _service.GetImageAsync(string.Empty);
+            });
+        }
+
+        [Test]
+        public async Task TestGetImagesAsync()
+        {
+            var images = await _service.GetImagesAsync("559d0e88e4b03bd51879a0de", "559d0e88e4b0a35bc506fbf8");
+            Assert.AreEqual(images.ErrorCode, 0);
+            Assert.AreEqual(images.Results.Count, 2);
+            Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await _service.GetImagesAsync(null);
+            });
+        }
     }
 }
