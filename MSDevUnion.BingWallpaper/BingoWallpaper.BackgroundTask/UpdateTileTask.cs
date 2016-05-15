2a4205da92789b1b83853e2b16dc32185e1d64bb
h82258652 <h82258652@hotmail.com>
2016-05-15 22:46:10 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.BackgroundTask/UpdateTileTask.cs
@@ -0,0 +1,19 @@
+﻿using Windows.ApplicationModel.Background;
+
+namespace BingoWallpaper.BackgroundTask
+{
+    public sealed class UpdateTileTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+            try
+            {
+            }
+            finally
+            {
+                deferral.Complete();
+            }
+        }
+    }
+}
---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.UWP/View/NavigationBackRequestedEventArgs.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Windows.Phone.UI.Input;
+using Windows.UI.Core;
+
+namespace BingoWallpaper.Uwp.View
+{
+    public class NavigationBackRequestedEventArgs : EventArgs
+    {
+        private readonly BackPressedEventArgs _backPressedEventArgs;
+
+        private readonly BackRequestedEventArgs _backRequestedEventArgs;
+
+        internal NavigationBackRequestedEventArgs(BackRequestedEventArgs e)
+        {
+            if (e == null)
+            {
+                throw new ArgumentNullException(nameof(e));
+            }
+
+            _backRequestedEventArgs = e;
+        }
+
+        internal NavigationBackRequestedEventArgs(BackPressedEventArgs e)
+        {
+            if (e == null)
+            {
+                throw new ArgumentNullException(nameof(e));
+            }
+
+            _backPressedEventArgs = e;
+        }
+
+        public bool Handled
+        {
+            get
+            {
+                if (_backRequestedEventArgs != null)
+                {
+                    return _backRequestedEventArgs.Handled;
+                }
+
+                if (_backPressedEventArgs != null)
+                {
+                    if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.BackPressedEventArgs"))
+                    {
+                        return _backPressedEventArgs.Handled;
+                    }
+                }
+                throw new InvalidOperationException();
+            }
+            set
+            {
+                if (_backRequestedEventArgs != null)
+                {
+                    _backRequestedEventArgs.Handled = value;
+                }
+                if (_backPressedEventArgs != null)
+                {
+                    if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.Phone.UI.Input.BackPressedEventArgs"))
+                    {
+                        _backPressedEventArgs.Handled = value;
+                    }
+                }
+            }
+        }
+    }
+}
