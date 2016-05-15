95bb94ee24c16bc0fac5e0bdd04c103c0a10c2e1
h82258652 <h82258652@hotmail.com>
2016-05-15 18:41:57 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Model/Model/Wallpaper.cs
@@ -0,0 +1,17 @@
+﻿namespace BingoWallpaper.Model
+{
+    public class Wallpaper
+    {
+        public Archive Archive
+        {
+            get;
+            set;
+        }
+
+        public Image Image
+        {
+            get;
+            set;
+        }
+    }
+}
