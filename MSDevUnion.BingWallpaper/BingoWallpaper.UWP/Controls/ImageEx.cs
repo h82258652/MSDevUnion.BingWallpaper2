ee32477c1b92429dcd05fa687a9bd2895fd87647
h82258652 <h82258652@hotmail.com>
2016-05-19 18:05:40 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Controls/ImageEx.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Controls/ImageEx.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
+using Windows.Web.Http;
 
 namespace BingoWallpaper.Uwp.Controls
 {
@@ -21,6 +25,16 @@ namespace BingoWallpaper.Uwp.Controls
         public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(nameof(Source), typeof(string), typeof(ImageEx), new PropertyMetadata(null, SourceChanged));
 
         public static readonly DependencyProperty StretchProperty = DependencyProperty.Register(nameof(Stretch), typeof(Stretch), typeof(ImageEx), new PropertyMetadata(Stretch.Uniform));
+
+        public static readonly DependencyProperty DownloadPercentProperty = DependencyProperty.Register(nameof(DownloadPercent), typeof(double), typeof(ImageEx), new PropertyMetadata(0.0d));
+
+        public double DownloadPercent
+        {
+            get
+            {
+                return (double)GetValue(DownloadPercentProperty);
+            }
+        }
 
         public ImageEx()
         {
@@ -60,58 +74,93 @@ namespace BingoWallpaper.Uwp.Controls
             UpdateSource(Source);
         }
 
+        private async void UpdateHttpSource(Uri uri)
+        {
+            IBuffer buffer;
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    var task = client.GetBufferAsync(uri);
+                    task.Progress = (info, progressInfo) =>
+                    {
+                        // TODO
+                        // Downloading.
+
+                        DownloadProgress?.Invoke(this, new DownloadProgressEventArgs());
+                    };
+                    buffer = await task;
+                }
+                catch (Exception ex)
+                {
+                    // TODO
+                    // Download Failed.
+
+                    _image.Visibility = Visibility.Visible;
+                    _placeholderContentControl.Visibility = Visibility.Collapsed;
+                    ImageFailed?.Invoke(this, new ExceptionRoutedEventArgs(ex));
+                    return;
+                }
+            }
+
+            var bitmap = new BitmapImage();
+            await bitmap.SetSourceAsync(buffer.AsStream().AsRandomAccessStream());
+            _image.Source = bitmap;
+            _image.Visibility = Visibility.Visible;
+            _placeholderContentControl.Visibility = Visibility.Collapsed;
+        }
+
+        private static bool IsHttpUri(Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            return uri.IsAbsoluteUri && (uri.Scheme == "http" || uri.Scheme == "https");
+        }
+
         private void UpdateSource(string source)
         {
             if (_image != null && _placeholderContentControl != null)
             {
-                //if (source == null)
-                //{
-                //    _image.Source = null;
-
-                //}
-                //else
-                //{
-                //    Uri uri;
-                //    if (Uri.TryCreate(source, UriKind.Absolute, out uri))
-                //    {
-                //        if (uri.Scheme == "http" || uri.Scheme == "https")
-                //        {
-                //            UpdateHttpSource(uri);
-                //            return;
-                //        }
-                //    }
-                //    else
-                //    {
-                //        Uri.TryCreate("ms-appx:///" + source, UriKind.Absolute, out uri);
-                //    }
-                //}
-
-                Uri uri;
-                if (source == null || Uri.TryCreate(source, UriKind.Absolute, out uri) == false)
+                if (source == null)
                 {
                     _image.Source = null;
-                    _image.Visibility = Visibility.Visible;
-                    _placeholderContentControl.Visibility = Visibility.Collapsed;
                 }
                 else
                 {
-                    var bitmap = new BitmapImage();
-                    bitmap.DownloadProgress += (sender, e) =>
+                    Uri uri;
+                    if (Uri.TryCreate(source, UriKind.RelativeOrAbsolute, out uri))
                     {
-                        DownloadProgress?.Invoke(this, e);
-                    };
-                    bitmap.ImageOpened += (sender, e) =>
+                        if (IsHttpUri(uri))
+                        {
+                            UpdateHttpSource(uri);
+                        }
+                        else
+                        {
+                            if (uri.IsAbsoluteUri == false)
+                            {
+                                Uri.TryCreate("ms-appx:///" + (source.StartsWith("/") ? source.Substring(1) : source), UriKind.Absolute, out uri);
+                            }
+
+                            UpdateLocalSource(uri);
+                        }
+                    }
+                    else
                     {
-                        ImageOpened?.Invoke(this, e);
-                    };
-                    bitmap.ImageFailed += (sender, e) =>
-                    {
-                        ImageFailed?.Invoke(this, e);
-                    };
-                    bitmap.UriSource = uri;
-                    _image.Source = bitmap;
+                        _image.Source = null;
+                    }
                 }
             }
+        }
+
+        private void UpdateLocalSource(Uri uri)
+        {
+            // TODO
+
+            BitmapImage bitmap = new BitmapImage(uri);
+            _image.Source = bitmap;
         }
 
         public event ExceptionRoutedEventHandler ImageFailed;
