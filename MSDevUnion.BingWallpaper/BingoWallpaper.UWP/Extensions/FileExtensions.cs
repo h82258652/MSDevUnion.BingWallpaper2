91b9863117480c023574b9e4733cd020881f8425
h82258652 <h82258652@hotmail.com>
2016-06-09 17:54:20 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Extensions/FileExtensions.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Extensions/FileExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Concurrent;
+﻿using BingoWallpaper.Utils;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -6,41 +6,17 @@ namespace BingoWallpaper.Uwp.Extensions
 {
     public static class FileExtensions
     {
-        private static readonly ConcurrentDictionary<string, TaskCompletionSource<object>> LockTcses = new ConcurrentDictionary<string, TaskCompletionSource<object>>();
-
         public static async Task WriteAllBytesAsync(string path, byte[] bytes)
         {
-            TaskCompletionSource<object> tcs;
-            while (true)
+            using (await StringTaskLocker.GetLockerAsync(path))
             {
-                if (LockTcses.TryGetValue(path, out tcs))
+                await Task.Run(() =>
                 {
-                    await tcs.Task;
-                }
-                else
-                {
-                    break;
-                }
+                    var directory = Path.GetDirectoryName(path);
+                    Directory.CreateDirectory(directory);
+                    File.WriteAllBytes(path, bytes);
+                });
             }
-
-            while (true)
-            {
-                tcs = new TaskCompletionSource<object>();
-                if (LockTcses.TryAdd(path, tcs))
-                {
-                    break;
-                }
-            }
-
-            await Task.Run(() =>
-            {
-                var directory = Path.GetDirectoryName(path);
-                Directory.CreateDirectory(directory);
-                File.WriteAllBytes(path, bytes);
-            });
-
-            tcs.SetResult(null);
-            LockTcses.TryRemove(path, out tcs);
         }
     }
 }
