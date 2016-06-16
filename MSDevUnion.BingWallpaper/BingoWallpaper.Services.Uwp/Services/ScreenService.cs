cf78ea903b63ebafaa3bb06063bea1828427bd78
h82258652 <h82258652@hotmail.com>
2016-06-16 12:54:37 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Services.Uwp/Services/ScreenService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml.Controls;
+
+namespace BingoWallpaper.Services
+{
+    public class ScreenService : IScreenService
+    {
+        public async Task<int> GetWidthAsync()
+        {
+            var webView = new WebView(WebViewExecutionMode.SeparateThread);
+            var width = int.Parse(await webView.InvokeScriptAsync("eval", new[]
+            {
+                "window.screen.width.toString()"
+            }));
+            var scale = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            return (int)Math.Ceiling(width * scale);
+        }
+
+        public async Task<int> GetHeightAsync()
+        {
+            var webView = new WebView(WebViewExecutionMode.SeparateThread);
+            var height = int.Parse(await webView.InvokeScriptAsync("eval", new[]
+            {
+                "window.screen.height.toString()"
+            }));
+            var scale = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            return (int)Math.Ceiling(height * scale);
+        }
+    }
+}
---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.Services/Services/IScreenService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace BingoWallpaper.Services
+{
+    public interface IScreenService
+    {
+        Task<int> GetWidthAsync();
+
+        Task<int> GetHeightAsync();
+    }
+}
