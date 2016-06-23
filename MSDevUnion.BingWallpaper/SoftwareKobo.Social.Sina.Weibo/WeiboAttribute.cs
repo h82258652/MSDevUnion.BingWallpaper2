ffd801ca58bc1ce03623623bfcc16c524ce6576f
h82258652 <h82258652@hotmail.com>
2016-06-23 20:29:12 +0800

    update

---
+++ MSDevUnion.BingWallpaper/SoftwareKobo.Social.Sina.Weibo/WeiboAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SoftwareKobo.Social.Sina.Weibo
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class WeiboAttribute : Attribute
+    {
+        public WeiboAttribute(string appkey, string appSecret, string redirectUri)
+        {
+            AppKey = appkey;
+            AppSecret = appSecret;
+            RedirectUri = redirectUri;
+        }
+
+        public string AppKey
+        {
+            get;
+            set;
+        }
+
+        public string AppSecret
+        {
+            get;
+            set;
+        }
+
+        public string RedirectUri
+        {
+            get;
+            set;
+        }
+    }
+}
