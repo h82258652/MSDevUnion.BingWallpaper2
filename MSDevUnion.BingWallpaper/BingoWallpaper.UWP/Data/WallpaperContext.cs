8bb2564f093b7bacbdeb2db3c29337dd3c4cbd6f
h82258652 <h82258652@hotmail.com>
2016-06-28 17:12:24 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Data/WallpaperContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BingoWallpaper.Uwp.Data
+{
+    //public class WallpaperContext : DbContext
+    //{
+    //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    {
+    //        base.OnConfiguring(optionsBuilder);
+    //    }
+    //}
+}
