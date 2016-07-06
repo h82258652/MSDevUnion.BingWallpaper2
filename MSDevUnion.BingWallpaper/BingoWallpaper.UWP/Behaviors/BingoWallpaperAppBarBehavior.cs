4d86f82eed1d7602fdfd820b94e9fbb38b2bf68a
h82258652 <h82258652@hotmail.com>
2016-07-06 15:59:15 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Behaviors/BingoWallpaperAppBarBehavior.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml.Controls;
+
+namespace BingoWallpaper.Uwp.Behaviors
+{
+    public class BingoWallpaperAppBarBehavior : Behavior<AppBar>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+        }
+    }
+}
