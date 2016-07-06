b994c8f91eac2c65c618e39932d0eadd0f7b5535
h82258652 <h82258652@hotmail.com>
2016-07-06 17:59:48 +0800

    update

---
+++ MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Controls/ImageEx2.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Windows.Media.Casting;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace BingoWallpaper.Uwp.Controls
+{
+    [TemplatePart(Name = ImageTemplateName, Type = typeof(Image))]
+    public class ImageEx2 : Control
+    {
+        public static readonly DependencyProperty PlaceholderTemplateProperty = DependencyProperty.Register(nameof(PlaceholderTemplate), typeof(DataTemplate), typeof(ImageEx2), new PropertyMetadata(null));
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(nameof(Source), typeof(string), typeof(ImageEx2), new PropertyMetadata(null, SourceChanged));
+
+        public static readonly DependencyProperty StretchProperty = DependencyProperty.Register(nameof(Stretch), typeof(Stretch), typeof(ImageEx2), new PropertyMetadata(Stretch.Uniform));
+
+        public static DependencyProperty NineGridProperty = DependencyProperty.Register(nameof(NineGrid), typeof(Thickness), typeof(ImageEx2), new PropertyMetadata(default(Thickness)));
+
+        private const string ImageTemplateName = "PART_Image";
+
+        private Image _image;
+
+        public ImageEx2()
+        {
+            DefaultStyleKey = typeof(ImageEx2);
+        }
+
+        public Thickness NineGrid
+        {
+            get
+            {
+                return (Thickness)GetValue(NineGridProperty);
+            }
+            set
+            {
+                SetValue(NineGridProperty, value);
+            }
+        }
+
+        public DataTemplate PlaceholderTemplate
+        {
+            get
+            {
+                return (DataTemplate)GetValue(PlaceholderTemplateProperty);
+            }
+            set
+            {
+                SetValue(PlaceholderTemplateProperty, value);
+            }
+        }
+
+        public string Source
+        {
+            get
+            {
+                return (string)GetValue(SourceProperty);
+            }
+            set
+            {
+                SetValue(SourceProperty, value);
+            }
+        }
+
+        public Stretch Stretch
+        {
+            get
+            {
+                return (Stretch)GetValue(StretchProperty);
+            }
+            set
+            {
+                SetValue(StretchProperty, value);
+            }
+        }
+
+        public CastingSource GetAsCastingSource()
+        {
+            return _image?.GetAsCastingSource();
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _image = (Image)GetTemplateChild(ImageTemplateName);
+        }
+
+        private static bool IsHttpUri(Uri uri)
+        {
+            if (uri == null)
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            return uri.IsAbsoluteUri && (uri.Scheme == "http" || uri.Scheme == "https");
+        }
+
+        private static void SourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
