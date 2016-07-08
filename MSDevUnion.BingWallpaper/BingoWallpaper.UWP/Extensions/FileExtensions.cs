e8753258c7da99cf23df1174757a30e0674dba09
h82258652 <h82258652@hotmail.com>
2016-07-08 13:13:28 +0800

    update

--- MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Extensions/FileExtensions.cs
+++ MSDevUnion.BingWallpaper/BingoWallpaper.UWP/Extensions/FileExtensions.cs
@@ -7,6 +7,26 @@ namespace BingoWallpaper.Uwp.Extensions
 {
     public static class FileExtensions
     {
+        public static Task<byte[]> ReadAllBytesAsync(string path)
+        {
+            return ReadAllBytesAsync(path, CancellationToken.None);
+        }
+
+        public static async Task<byte[]> ReadAllBytesAsync(string path, CancellationToken cancellationToken)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            using (var fs = File.OpenRead(path))
+            {
+                var buffer = new byte[fs.Length];
+                await fs.ReadAsync(buffer, 0, buffer.Length, cancellationToken);
+                return buffer;
+            }
+        }
+
         public static Task WriteAllBytesAsync(string path, byte[] bytes)
         {
             return WriteAllBytesAsync(path, bytes, CancellationToken.None);
